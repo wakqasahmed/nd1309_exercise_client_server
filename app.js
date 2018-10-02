ec39ecc1269122f89d804a7a02ae07f39db2fbdb
Jose Perera Morales <jpmorales@jose-perera-morales.fios-router.home>
2018-10-02 15:26:14 -0400

    -

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const http = require('http');
 
 //Step 1. Import crypto-js/sha256
-const SHA256 = require('crypto-js/sha256');
+
 
 // Http port
 const port = 8080;
@@ -19,13 +19,10 @@ blocks.push(block_2);
 /**
  * Take the block_2 data from the array "blocks" and generate the hash to be written into the response.
  */
-const app = http.createServer(function (request, response){
-    response.writeHead(200, {"Content-Type": "application/json"});
-    let block = blocks[1];
-    // Add your code here
-    response.write(SHA256(JSON.stringify(block)).toString());
-    response.end();
-});
+//Add your code here
+
+
+
 // Notify console
 console.log("Web Server started on port 8080\nhttp://localhost:"+port);
 // Start server with http port
